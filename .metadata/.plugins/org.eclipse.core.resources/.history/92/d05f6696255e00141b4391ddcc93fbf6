5757f6c29fa7a3d47c60ef9dbb854185bf02ba5c
Sam Kreter <sakfy6@mail.missouri.edu>
2014-10-27 17:08:24 -0500

    finished first inheritance structure

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/92/d05f6696255e00141b4391ddcc93fbf6
@@ -0,0 +1,15 @@
+package sakfy6.cs3330.lab7;
+
+public class Snake implements NonFlying{
+
+	
+	Snake(String type){
+		
+	}
+	public boolean eat(Animal food){
+		
+	}
+	public void movement(){
+		
+	}
+}
--- sakfy6.cs3330.lab7/src/sakfy6/cs3330/lab7/Snake.java
+++ sakfy6.cs3330.lab7/src/sakfy6/cs3330/lab7/Snake.java
@@ -1,15 +1,15 @@
 package sakfy6.cs3330.lab7;
 
-public class Snake implements NonFlying{
+public class Snake extends Animal implements NonFlying{
 
 	
 	Snake(String type){
-		
+		super(false,false,type);
 	}
 	public boolean eat(Animal food){
-		
+		return false;
 	}
 	public void movement(){
-		
+		System.out.println("The snake slithers on the ground");
 	}
 }
